f0447c82d86c9422975e7a636c0ff3b99f8b808b
gleisson <gleissonfreitas@hotmail.com>
2017-10-02 21:26:24 -0300

    //Commit final

--- src/TesteTradutor.java
+++ src/TesteTradutor.java
@@ -1,22 +1,69 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TesteTradutor {
 //
+	private Tradutor t;
+	
+	@Before
+	public void criarTradutor(){
+		t = new Tradutor();
+	}
+	
 	@Test
 	public void tradutorSemPalavras() {
-		Tradutor t = new Tradutor();
+		t = new Tradutor();
 		assertTrue(t.estaVazio());
 	}
 	
 	@Test
 	public void umaTraducao(){
-		Tradutor t = new Tradutor();
+		t = new Tradutor();
 		t.adicionaTraducao("bom","good");
 		assertFalse(t.estaVazio());
 		assertEquals("good", t.traduzir("bom"));
+		t.adicionaTraducao("mau","bad");
+	}
+	
+	@Test
+	public void duasTraducoes(){
+		t = new Tradutor();
+		t.adicionaTraducao("bom","good");
+		t.adicionaTraducao("mau","bad");
+		assertEquals("good", t.traduzir("bom"));
+		assertEquals("bad", t.traduzir("mau"));
+	}
+	
+	@Test
+	public void duasTraducoesMesmaPalavra(){
+		t = new Tradutor();
+		t.adicionaTraducao("bom","good");
+		t.adicionaTraducao("bom","nice");
+		assertEquals("good, nice", t.traduzir("bom"));
+	}
+	
+	@Test
+	public void traduzirFrase(){
+		t = new Tradutor();
+		t.adicionaTraducao("guerra","war");
+		t.adicionaTraducao("é","is");
+		t.adicionaTraducao("ruim","bad");
+		assertEquals("war is bad", t.traduzirFrase("guerra é ruim"));
+		
+	}
+	
+	@Test
+	public void traduzirFraseComDuasTraducoesMesmaPalavra(){
+		t = new Tradutor();
+		t.adicionaTraducao("paz","peace");
+		t.adicionaTraducao("é","is");
+		t.adicionaTraducao("bom","good");
+		t.adicionaTraducao("bom","nice");
+		assertEquals("peace is good", t.traduzirFrase("paz é bom"));
+		
 	}
 
 }
--- src/Tradutor.java
+++ src/Tradutor.java
@@ -1,19 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Tradutor {
 //
-	private String traducao;
+	private Map<String,String> traducoes = new HashMap<>();
 	
-	public void adicionaTraducao(String palavra, String traducao) {
-		this.traducao = traducao;
-		
-	}
+	
 
 	public boolean estaVazio() {
 		
-		return (traducao==null);
+		return traducoes.isEmpty();
+	}
+	
+	public void adicionaTraducao(String palavra, String traducao) {
+		if (traducoes.containsKey(palavra)){
+			traducao = traduzir(palavra)+", "+traducao; 
+		}
+		traducoes.put(palavra, traducao);
+		
 	}
 
-	public Object traduzir(String palavra) {
+	public String traduzir(String palavra) {
+		return traducoes.get(palavra);
+	}
+
+	public String traduzirFrase(String frase) {
+		String[] palavras = frase.split(" ");
+		String fraseTraduzida = "";
+		for(String palavra: palavras){
+			String traducao = primeiraTraducao(palavra);
+			fraseTraduzida += " "+traducao;
+		}
+		
+		return fraseTraduzida.trim();
+	}
+
+	private String primeiraTraducao(String palavra) {
+		String traducao = traduzir(palavra);
+		if(traducao.contains(","))
+			traducao = traducao.substring(0, traducao.indexOf(","));
 		return traducao;
 	}
 
